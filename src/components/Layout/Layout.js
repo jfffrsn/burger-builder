9d3fd96ae2bd10c6a26868eb96bd218cb0edac53
jeffferson99 <49041684+jeffferson99@users.noreply.github.com>
2019-12-14 23:48:04 -0500

    init

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Auxillary from "../../hoc/Auxillary";
+
+import classes from '../Layout/Layout.module.css'
+
+
+const layout = props => (
+  <Auxillary>
+    <div>
+        Toolbar Sidedrawer Backdrop</div>
+    <main className={classes.Content}>
+        {props.children}
+        </main>
+  </Auxillary>
+);
+
+export default layout;
