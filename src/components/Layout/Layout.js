85ff920774ea778ced36f0a267b5ee0e4694b9dc
jeffferson99 <49041684+jeffferson99@users.noreply.github.com>
2019-12-15 01:00:48 -0500

    cleanup

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,15 @@
 import React from "react";
 import Auxillary from "../../hoc/Auxillary";
 
-import classes from '../Layout/Layout.module.css';
+import classes from "../Layout/Layout.module.css";
 
-import Toolbar from '../Navigation/Toolbar/Toolbar';
+import Toolbar from "../Navigation/Toolbar/Toolbar";
 
 const layout = props => (
   <Auxillary>
     <Toolbar />
-    <div>
-        Toolbar Sidedrawer Backdrop</div>
-    <main className={classes.Content}>
-        {props.children}
-        </main>
+    <div>Toolbar Sidedrawer Backdrop</div>
+    <main className={classes.Content}>{props.children}</main>
   </Auxillary>
 );
 
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import burgerLogo from '../../assets/images/burger-logo.png';
+import burgerLogo from "../../assets/images/burger-logo.png";
 
-import classes from './Logo.module.css';
+import classes from "./Logo.module.css";
 
-const Logo = (props) => {
-    return (
-        <div className={classes.Logo}>
-            <img src={burgerLogo} alt="MyBurger" />
-        </div>
-    );
+const Logo = props => {
+  return (
+    <div className={classes.Logo}>
+      <img src={burgerLogo} alt="MyBurger" />
+    </div>
+  );
 };
 
 export default Logo;
