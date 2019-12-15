6f3a594cf5e15e78bef2b8a3d4e76b1c2fe57a50
jeffferson99 <49041684+jeffferson99@users.noreply.github.com>
2019-12-15 02:51:09 -0500

    Creating a Responsive Sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,9 +5,12 @@ import classes from "../Layout/Layout.module.css";
 
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
+
 const layout = props => (
   <Auxillary>
     <Toolbar />
+    <SideDrawer />
     <div>Toolbar Sidedrawer Backdrop</div>
     <main className={classes.Content}>{props.children}</main>
   </Auxillary>
