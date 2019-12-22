e6c3ab833343031a9692871089f0f874a0d69dd7
jeffferson99 <49041684+jeffferson99@users.noreply.github.com>
2019-12-22 03:55:12 -0500

    reducer ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -31,15 +31,15 @@ class BurgerBuilder extends Component {
   };
 
   componentDidMount() {
-    console.log(this.props);
-    axios
-      .get("https://react-my-burger-4fdb3.firebaseio.com/ingredients.json")
-      .then(response => {
-        this.setState({ ingredients: response.data });
-      })
-      .catch(error => {
-        this.setState({ error: true });
-      });
+ //   console.log(this.props);
+ //   axios
+ //     .get("https://react-my-burger-4fdb3.firebaseio.com/ingredients.json")
+ //     .then(response => {
+ //       this.setState({ ingredients: response.data });
+ //     })
+ //     .catch(error => {
+ //       this.setState({ error: true });
+ //     });
   }
 
   updatePurchaseState(ingredients) {
